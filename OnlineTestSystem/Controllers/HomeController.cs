155d71c8f983b3c5299f05d4b74fed34caa5eeb1
Ibrar1998 <007igill@gmail.com>
2020-01-17 22:34:41 +0500

    Initial version of the code uploded

---
+++ OnlineTestSystem/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using OnlineTestSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTestSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Models.RegisterUser registerUser  )
+        {
+            OnlineTestSystemEntities db = new OnlineTestSystemEntities();
+            User u = new User();
+            u.Password = registerUser.Password;
+            u.Username = registerUser.Username;
+            u.Email = registerUser.Email;
+            u.Roleid = 2;
+            db.Users.Add(u);
+            db.SaveChanges();
+
+            RedirectToAction("","Home","Index");
+
+            return View();
+        }
+
+    }
+}
---
+++ OnlineTestSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineTestSystem.MvcApplication" Language="C#" %>
